b9721e7b4280bccd1a9915b2d7b46057d34770fa
Donald <448641125@qq.com>
2019-03-22 14:25:12 +0800

    add object reference

---
+++ test/test_reference.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+type Test struct {
+	Name  string
+	Age   int
+}
+
+// 探究对象赋值
+func main() {
+
+	test := Test{Name:"duoNa", Age:12}
+
+	testReference(test)
+}
+
+func testReference(test Test) {
+
+	fmt.Println(test)
+
+	str := "12321"
+
+	age := 123
+
+	test.Name = str
+
+	test.Age = age
+
+	fmt.Println(test)
+}
